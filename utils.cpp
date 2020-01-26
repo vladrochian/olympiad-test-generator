fabfbee87167970ea2223d17a2e0e869a467e19c
Vlad Rochian <vlad.rochian@gmail.com>
2020-01-26 21:13:05 +0200

    Add graph generation functions

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,14 @@
+#include <algorithm>
 #include <chrono>
+#include <random>
+#include <set>
 #include "utils.h"
 
 Utils::Utils() : generator(std::chrono::system_clock::now().time_since_epoch().count()) {
+}
+
+bool Utils::getRandomBool() {
+  return getRandomInt(0, 1) == 0;
 }
 
 int Utils::getRandomInt(int minValue, int maxValue) {
@@ -12,4 +19,61 @@ int Utils::getRandomInt(int minValue, int maxValue) {
 int64_t Utils::getRandomInt64(int64_t minValue, int64_t maxValue) {
   std::uniform_int_distribution<int64_t> distribution(minValue, maxValue);
   return distribution(generator);
+}
+
+std::vector<int> Utils::getShuffledSequence(int size, int startFrom) {
+  std::vector<int> seq(size);
+  for (int i = 0; i < size; ++i) {
+    seq[i] = i + startFrom;
+  }
+  std::shuffle(seq.begin(), seq.end(), generator);
+  return seq;
+}
+
+std::vector<std::pair<int, int>> Utils::getTree(int nodeCount, int startIndexFrom) {
+  std::vector<int> indices = getShuffledSequence(nodeCount, startIndexFrom);
+  std::vector<std::pair<int, int>> edges;
+  for (int i = 1; i < nodeCount; ++i) {
+    int parent = getRandomInt(0, i - 1);
+    edges.emplace_back(indices[parent], indices[i]);
+  }
+  std::shuffle(edges.begin(), edges.end(), generator);
+  return edges;
+}
+
+std::vector<std::pair<int, int>> Utils::getGraph(int nodeCount, int edgeCount, int startIndexFrom) {
+  return addEdgesToGraph(std::vector<std::pair<int, int>>(), nodeCount, edgeCount, startIndexFrom);
+}
+
+std::vector<std::pair<int, int>> Utils::getConnectedGraph(int nodeCount, int edgeCount, int startIndexFrom) {
+  auto edges = getTree(nodeCount, startIndexFrom);
+  return addEdgesToGraph(edges, nodeCount, edgeCount, startIndexFrom);
+}
+
+std::vector<std::pair<int, int>>
+Utils::addEdgesToGraph(const std::vector<std::pair<int, int>>& edges, int nodeCount, int totalEdgeCount,
+                       int startIndexFrom) {
+  std::set<std::pair<int, int>> edgeSet;
+  for (auto edge : edges) {
+    if (edge.first > edge.second) {
+      std::swap(edge.first, edge.second);
+    }
+    edgeSet.insert(edge);
+  }
+  while (static_cast<int>(edgeSet.size()) < totalEdgeCount) {
+    int x = getRandomInt(0, nodeCount - 1) + startIndexFrom;
+    int y = getRandomInt(0, nodeCount - 1) + startIndexFrom;
+    if (x > y) {
+      std::swap(x, y);
+    }
+    edgeSet.emplace(x, y);
+  }
+  std::vector<std::pair<int, int>> edgeList(edgeSet.begin(), edgeSet.end());
+  std::shuffle(edgeList.begin(), edgeList.end(), generator);
+  for (auto& edge : edgeList) {
+    if (getRandomBool()) {
+      std::swap(edge.first, edge.second);
+    }
+  }
+  return edgeList;
 }
--- utils.h
+++ utils.h
@@ -2,14 +2,23 @@
 #define OLYMPIAD_TEST_GENERATOR_UTILS_H
 
 #include <random>
+#include <vector>
 
 class Utils {
  public:
   Utils();
+  bool getRandomBool();
   int getRandomInt(int minValue, int maxValue);
   int64_t getRandomInt64(int64_t minValue, int64_t maxValue);
+  std::vector<int> getShuffledSequence(int size, int startFrom = 1);
+  std::vector<std::pair<int, int>> getTree(int nodeCount, int startIndexFrom = 1);
+  std::vector<std::pair<int, int>> getGraph(int nodeCount, int edgeCount, int startIndexFrom = 1);
+  std::vector<std::pair<int, int>> getConnectedGraph(int nodeCount, int edgeCount, int startIndexFrom = 1);
 
  private:
+  std::vector<std::pair<int, int>>
+  addEdgesToGraph(const std::vector<std::pair<int, int>>& edges, int nodeCount, int totalEdgeCount,
+                  int startIndexFrom = 1);
   std::default_random_engine generator;
 };
 
